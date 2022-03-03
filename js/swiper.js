a2c211898d5a5792f11dd4fa29a8e04612d4b8e9
EricVartanov <82024638+EricVartanov@users.noreply.github.com>
2022-03-03 17:22:57 +0300

    Add files via upload

--- js/swiper.js
+++ js/swiper.js
@@ -13,9 +13,23 @@ let InitSwiper = function (slider) {
         watchSlidesProgress: true,
         touchReleaseOnEdges: true,
         allowTouchMove: true,
-        grabCursor:true,
+        grabCursor: true,
         /*    grabCursor: true, */
         watchOverflow: true,
+        breakpoints: {
+            // when window width is >= 320px
+            320: {
+                spaceBetween: 5,
+            },
+            // when window width is >= 480px
+            700: {
+                spaceBetween: 10,
+            },
+            // when window width is >= 640px
+            1020: {
+                spaceBetween: 20,
+            },
+        },
         navigation: {
             nextEl: ".idc-swiper-button-next",
             prevEl: ".idc-swiper-button-prev",
@@ -33,7 +47,7 @@ let InitSwiper = function (slider) {
                         (index + 1) +
                         "</span> </span>"
                     );
-                } else{
+                } else {
                     return (
                         '<span class="' +
                         className +
